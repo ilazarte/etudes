91c74028f7de0d1c9a309e18fa0bc07df00f24da
ivan.lazarte@gmail.com <ilazarte@users.noreply.github.com>
2017-03-26 04:53:50 -0400

    added code in prep for roman numeral table

---
+++ src/audio/Pitch.ts
@@ -0,0 +1,27 @@
+enum Pitch {A, B, C, D, E, F, G}
+
+module Pitch {
+    export function toPitch(enc: string): Pitch {
+        if (!enc) {
+            return Pitch.C;
+        } else if (enc === "A") {
+            return Pitch.A;
+        } else if (enc === "B") {
+            return Pitch.B;
+        } else if (enc === "C") {
+            return Pitch.C;
+        } else if (enc === "D") {
+            return Pitch.D;
+        } else if (enc === "E") {
+            return Pitch.E;
+        } else if (enc === "F") {
+            return Pitch.F;
+        } else if (enc === "G") {
+            return Pitch.G;
+        } else {
+            throw `Invalid pitch: ${enc}`;
+        }
+    }
+}
+
+export {Pitch};
